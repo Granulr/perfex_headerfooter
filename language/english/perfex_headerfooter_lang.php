0a9eb5eed6c73d594975f194d318680a1cda4ca9
James Simpson <james.simpson@outrank.co.uk>
2020-11-05 22:59:09 +0000

    language updates

--- language/english/perfex_headerfooter_lang.php
+++ language/english/perfex_headerfooter_lang.php
@@ -4,3 +4,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 # Version 1.0.0
 $lang['headerfooter_javascript']            = 'Custom Javascript';
 $lang['headerfooter_adminheadjs']           = 'Custom Admin Head JS';
+$lang['headerfooter_adminfootjs']           = 'Custom Admin Footer JS';
+$lang['headerfooter_customerheadjs']        = 'Custom Customer Head JS';
+$lang['headerfooter_customerfootjs']        = 'Custom Customer Footer JS';
